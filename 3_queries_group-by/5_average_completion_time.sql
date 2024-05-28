d6db8724240a35bee7d88f15d3e360ffa73b5996
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-27 22:23:54 -0400

    'create query'

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,23 @@
+
+
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM assignment_submissions 
+JOIN students ON student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
+
+
+
+
+-- LHL solution
+-- SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+-- FROM assignment_submissions 
+-- JOIN students ON student_id = students.id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name
+-- ORDER BY average_assignment_duration DESC;
+
+
+
+
